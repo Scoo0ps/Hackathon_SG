ad069104fb7d3f4b35fec41261f5bf42e3a6a06d
mtslb <mathias.lebouedec2@gmail.com>
2025-10-16 11:12:09 +0200

    Refactor data fetching and processing in test.py to include percentage change calculation and consolidate DataFrames

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import yfinance as yf
+import pandas as pd
+
 
 # Société Générale (GLE.PA), Tesla (TSLA), Bitcoin (BTC-USD)
 tickers = [
@@ -28,6 +30,23 @@ tickers = [
 ]
 
 
+dfs = []
+
 for t in tickers:
-    prices = yf.download(t, period="7d", interval="1d")
-    print(prices)
+    df = yf.download(t, period="7d", interval="1d")
+    if df.empty:
+        continue
+    # Calcul variation en %
+    df["Pct_change"] = ((df["Close"] - df["Open"]) / df["Open"]) * 100
+    # Ajout d’une colonne ticker pour identifier les données
+    df["Ticker"] = t
+    # Sélection colonnes
+    df = df[["Ticker", "Open", "High", "Low", "Close", "Pct_change", "Volume"]]
+    # Reset index pour avoir la date en colonne
+    df = df.reset_index()
+    dfs.append(df)
+
+# Concaténer tous les DataFrames en un seul
+final_df = pd.concat(dfs, ignore_index=True)
+
+print(final_df.head(15))
